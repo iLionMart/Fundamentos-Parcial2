54829bbdb0281daad16ced23f0c91679b1bf1140
Lion Mart <45979134+LeonGamerGL@users.noreply.github.com>
2024-03-07 17:05:50 -0600

    Create 6_RangoClass.java

---
+++ 6_RangoClass.java
@@ -0,0 +1,12 @@
+public class main {
+    public static void main(String args[]){
+    
+        int x = 23;
+
+        if (x>=3 && x<=23){
+            System.out.println("El numero " + x + " esta dentro del rango");
+        }else{
+            System.out.println("El numero " + x + " no esta dentro del rango");
+        }
+    }
+}
