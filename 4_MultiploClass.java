d2897c025a47151068adeff3202b00ef7c6a55f5
Lion Mart <45979134+LeonGamerGL@users.noreply.github.com>
2024-03-07 16:55:28 -0600

    Create 4_MultiploClass.java

---
+++ 4_MultiploClass.java
@@ -0,0 +1,12 @@
+public class main {
+    public static void main(String args[]){
+        
+        int a = 15;
+        
+        if (a % 5 ==0){
+            System.out.println("El valor es multiplo de 5");
+        }else{
+            System.out.println("El valor no es multiplo de 5");
+        }
+    }
+} 
