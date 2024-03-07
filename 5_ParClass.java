32d12ada1f5eeb0c9fed75a8269ed262ca735bf5
Lion Mart <45979134+LeonGamerGL@users.noreply.github.com>
2024-03-07 16:56:07 -0600

    Create 5_ParClass.java

---
+++ 5_ParClass.java
@@ -0,0 +1,12 @@
+public class main {
+    public static void main(String args[]){
+        
+        int a = 8;
+        
+        if (a % 2 ==0){
+            System.out.println("El valor es par");
+        }else{
+            System.out.println("El valor no es par");
+        }
+    }
+}  
