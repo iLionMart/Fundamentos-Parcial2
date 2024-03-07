615ba9b4b560bdb29fdef51f0391a23bbe1ca781
Lion Mart <45979134+LeonGamerGL@users.noreply.github.com>
2024-03-07 17:49:39 -0600

    Create 8_HoraClass.java

---
+++ 8_HoraClass.java
@@ -0,0 +1,23 @@
+public class main {
+    public static void main(String[] args) {
+
+        int seg = 83;
+        int min = 42;
+        int hora = 21;
+        seg= seg + 1;
+
+        if (seg>=60){
+            min= min + 1;
+            seg= seg - 60;
+        }
+        if (min >=60){
+            hora= hora + 1;
+            min= min - 60;
+        }
+        if (hora >=24){
+            hora= 0;
+        }
+
+        System.out.println("La nueva hora tras es el segundo es: " + hora + " con " + min + " minutos " + " con " + seg + " segundos");
+    }
+}
