9d60327c3b398d0884c549c0d82953b0f6cdcdcc
Lion Mart <45979134+LeonGamerGL@users.noreply.github.com>
2024-03-07 19:21:23 -0600

    Create 14_NumALetClass.java

---
+++ 14_NumALetClass.java
@@ -0,0 +1,230 @@
+public class main {
+    public static void main(String[] args) {
+        
+        int x = 78;
+
+        if (x==0){
+            System.out.println("Tu numero es cero");
+        }else if (x>=1 && x<=10){
+            if(x==1){
+                System.out.println("Tu numero es uno");
+            }else if(x==2){
+                System.out.println("Tu numero es dos");
+            }else if(x==3){
+                System.out.println("Tu numero es tres");
+            }else if(x==4){
+                System.out.println("Tu numero es cuatro");
+            }else if(x==5){
+                System.out.println("Tu numero es cinco");
+            }else if(x==6){
+                System.out.println("Tu numero es seis");
+            }else if(x==7){
+                System.out.println("Tu numero es siete");
+            }else if(x==8){
+                System.out.println("Tu numero es ocho");
+            }else if(x==9){
+                System.out.println("Tu numero es nueve");
+            }else if(x==10){
+                System.out.println("Tu numero es diez");
+            }
+        }else if (x>=11 && x<=20){
+            if(x==11){
+                System.out.println("Tu numero es once");
+            }else if(x==12){
+                System.out.println("Tu numero es doce");
+            }else if(x==13){
+                System.out.println("Tu numero es trece");
+            }else if(x==14){
+                System.out.println("Tu numero es catorce");
+            }else if(x==15){
+                System.out.println("Tu numero es quince");
+            }else if(x==16){
+                System.out.println("Tu numero es dieciseis");
+            }else if(x==17){
+                System.out.println("Tu numero es diecisiete");
+            }else if(x==18){
+                System.out.println("Tu numero es dieciocho");
+            }else if(x==19){
+                System.out.println("Tu numero es diecinueve");
+            }else if(x==20){
+                System.out.println("Tu numero es viente");
+            }
+        }else if (x>=21 && x<=30){
+            if(x==21){
+                System.out.println("Tu numero es veintiuno");
+            }else if(x==22){
+                System.out.println("Tu numero es veintidos");
+            }else if(x==23){
+                System.out.println("Tu numero es veintitres");
+            }else if(x==24){
+                System.out.println("Tu numero es veinticuatro");
+            }else if(x==25){
+                System.out.println("Tu numero es veinticinco");
+            }else if(x==26){
+                System.out.println("Tu numero es veintiseis");
+            }else if(x==27){
+                System.out.println("Tu numero es veintisiete");
+            }else if(x==28){
+                System.out.println("Tu numero es veintiocho");
+            }else if(x==29){
+                System.out.println("Tu numero es veintinueve");
+            }else if(x==30){
+                System.out.println("Tu numero es treinta");
+            }
+        }else if (x>=31 && x<=40){
+            if(x==31){
+                System.out.println("Tu numero es treinta y uno");
+            }else if(x==32){
+                System.out.println("Tu numero es treinta y dos");
+            }else if(x==33){
+                System.out.println("Tu numero es treinta y tres");
+            }else if(x==34){
+                System.out.println("Tu numero es treinta y cuatro");
+            }else if(x==35){
+                System.out.println("Tu numero es treinta y cinco");
+            }else if(x==36){
+                System.out.println("Tu numero es treinta y seis");
+            }else if(x==37){
+                System.out.println("Tu numero es treinta y siete");
+            }else if(x==38){
+                System.out.println("Tu numero es treinta y ocho");
+            }else if(x==39){
+                System.out.println("Tu numero es treinta y nueve");
+            }else if(x==40){
+                System.out.println("Tu numero es cuarenta");
+            }
+        }else if (x>=41 && x<=50){
+            if(x==41){
+                System.out.println("Tu numero es cuarenta y uno");
+            }else if(x==42){
+                System.out.println("Tu numero es cuarenta y dos");
+            }else if(x==43){
+                System.out.println("Tu numero es cuarenta y tres");
+            }else if(x==44){
+                System.out.println("Tu numero es cuarenta y cuatro");
+            }else if(x==45){
+                System.out.println("Tu numero es cuarenta y cinco");
+            }else if(x==46){
+                System.out.println("Tu numero es cuarenta y seis");
+            }else if(x==47){
+                System.out.println("Tu numero es cuarenta y siete");
+            }else if(x==48){
+                System.out.println("Tu numero es cuarenta y ocho");
+            }else if(x==49){
+                System.out.println("Tu numero es cuarenta y nueve");
+            }else if(x==50){
+                System.out.println("Tu numero es cincuenta");
+            }
+        }else if (x>=51 && x<=60){
+            if(x==51){
+                System.out.println("Tu numero es cincuenta y uno");
+            }else if(x==52){
+                System.out.println("Tu numero es cincuenta y dos");
+            }else if(x==53){
+                System.out.println("Tu numero es cincuenta y tres");
+            }else if(x==54){
+                System.out.println("Tu numero es cincuenta y cuatro");
+            }else if(x==55){
+                System.out.println("Tu numero es cincuenta y cinco");
+            }else if(x==56){
+                System.out.println("Tu numero es cincuenta y seis");
+            }else if(x==57){
+                System.out.println("Tu numero es cincuenta y siete");
+            }else if(x==58){
+                System.out.println("Tu numero es cincuenta y ocho");
+            }else if(x==59){
+                System.out.println("Tu numero es cincuenta y nueve");
+            }else if(x==60){
+                System.out.println("Tu numero es sesenta");
+            }
+        }else if (x>=61 && x<=70){
+            if(x==61){
+                System.out.println("Tu numero es sesenta y uno");
+            }else if(x==62){
+                System.out.println("Tu numero es sesenta y dos");
+            }else if(x==63){
+                System.out.println("Tu numero es sesenta y tres");
+            }else if(x==64){
+                System.out.println("Tu numero es sesenta y cuatro");
+            }else if(x==65){
+                System.out.println("Tu numero es sesenta y cinco");
+            }else if(x==66){
+                System.out.println("Tu numero es sesenta y seis");
+            }else if(x==67){
+                System.out.println("Tu numero es sesenta y siete");
+            }else if(x==68){
+                System.out.println("Tu numero es sesenta y ocho");
+            }else if(x==69){
+                System.out.println("Tu numero es sesenta y nueve");
+            }else if(x==70){
+                System.out.println("Tu numero es setenta");
+            }
+        }else if (x>=1 && x<=10){
+            if(x==71){
+                System.out.println("Tu numero es setenta y uno");
+            }else if(x==72){
+                System.out.println("Tu numero es setenta y dos");
+            }else if(x==73){
+                System.out.println("Tu numero es setenta y tres");
+            }else if(x==74){
+                System.out.println("Tu numero es setenta y cuatro");
+            }else if(x==75){
+                System.out.println("Tu numero es setenta y cinco");
+            }else if(x==76){
+                System.out.println("Tu numero es setenta y seis");
+            }else if(x==77){
+                System.out.println("Tu numero es setenta y siete");
+            }else if(x==78){
+                System.out.println("Tu numero es setenta y ocho");
+            }else if(x==79){
+                System.out.println("Tu numero es setenta y nueve");
+            }else if(x==80){
+                System.out.println("Tu numero es ochenta");
+            }
+        }else if (x>=1 && x<=10){
+            if(x==81){
+                System.out.println("Tu numero es ochenta y uno");
+            }else if(x==82){
+                System.out.println("Tu numero es ochenta y dos");
+            }else if(x==83){
+                System.out.println("Tu numero es ochenta y tres");
+            }else if(x==84){
+                System.out.println("Tu numero es ochenta y cuatro");
+            }else if(x==85){
+                System.out.println("Tu numero es ochenta y cinco");
+            }else if(x==86){
+                System.out.println("Tu numero es ochenta y seis");
+            }else if(x==87){
+                System.out.println("Tu numero es ochenta y siete");
+            }else if(x==88){
+                System.out.println("Tu numero es ochenta y ocho");
+            }else if(x==89){
+                System.out.println("Tu numero es ochenta y nueve");
+            }else if(x==90){
+                System.out.println("Tu numero es noventa");
+            }
+        }else if (x>=1 && x<=10){
+            if(x==91){
+                System.out.println("Tu numero es noventa y uno");
+            }else if(x==92){
+                System.out.println("Tu numero es noventa y dos");
+            }else if(x==93){
+                System.out.println("Tu numero es noventa y tres");
+            }else if(x==94){
+                System.out.println("Tu numero es noventa y cuatro");
+            }else if(x==95){
+                System.out.println("Tu numero es noventa y cinco");
+            }else if(x==96){
+                System.out.println("Tu numero es noventa y seis");
+            }else if(x==97){
+                System.out.println("Tu numero es noventa y siete");
+            }else if(x==98){
+                System.out.println("Tu numero es noventa y ocho");
+            }else if(x==99){
+                System.out.println("Tu numero es noventa y nueve");
+            }else if(x==100){
+                System.out.println("Tu numero es cien");
+            }
+        }
+    }
+}
