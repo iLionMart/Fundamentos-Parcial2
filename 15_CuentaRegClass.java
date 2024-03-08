e3b56f5a182ae6d4a2edfd4e85f01bb2775a0dfc
Lion Mart <45979134+LeonGamerGL@users.noreply.github.com>
2024-03-07 19:28:30 -0600

    Create 15_CuentaRegClass.java

---
+++ 15_CuentaRegClass.java
@@ -0,0 +1,13 @@
+public class main {
+    public static void main(String[] args) {
+
+        int n = 30;
+
+        System.out.println("Valor inicial " + n);
+
+        while (n>=1){
+            n= n-1;
+            System.out.println("Siguiente valor " + n);
+        }
+    }
+}
