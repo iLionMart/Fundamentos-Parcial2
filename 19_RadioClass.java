ef25b1a949d0df1b3807faec225107aaf4afec3b
Lion Mart <45979134+LeonGamerGL@users.noreply.github.com>
2024-03-20 19:26:10 -0600

    Create 19_RadioClass.java

---
+++ 19_RadioClass.java
@@ -0,0 +1,11 @@
+public class main {
+    public static void main(String args[]){
+        
+        int radio = 5;
+        double area = 0;
+        
+        area = Math.PI * (Math.pow(radio, 2));
+
+        System.out.println("El area del circulo es " + area);
+    }
+} 
